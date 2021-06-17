1653eb73b3861da66fd4f1fd3d7f4c3e17e11542
manish27dash <58607110+manish27dash@users.noreply.github.com>
2021-06-17 20:03:21 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,12 +3,15 @@
 
 filenum=$(ls | wc -l)
 
+# 1. a function
 function guess_filenum {
 	read guess
+	# 2. an if condition
 	if [[ $guess -eq $filenum ]]
 	then
 		echo "You guessed it!"
 		echo "  So we have ..."
+		# 3. for loop show up all 2 files available in the repository
 		for f in $(ls)
 		do
 			echo "  - $f and "
